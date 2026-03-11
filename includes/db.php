575e13e15385bc98cc35ffe57d41741b80ecb6e8
CodeCrizzz <natividadchristian670@gmail.com>
2026-03-11 22:41:57 +0800

    update database connection details for production environment

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Manila');
 
-$servername = "localhost"; //"sql103.infinityfree.com"; 
-$username   = "root"; //"if0_40612478";
-$password   = ""; //"oDunxhXQGpur";
-$dbname     = "boarding_house_db"; //"if0_40612478_bhms";
+$servername = "	sql103.infinityfree.com";
+$username   = "if0_40612478";
+$password   = "oDunxhXQGpur";
+$dbname     = "if0_40612478_boarding_house_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
